16d6dfa52aca30ab03b8ea5256c4101c87542cb2
Dominic Bowden <tedle@blon.se>
2019-12-05 14:27:52 -0800

    Refactor Day 5 code
    Have a hunch this cpu emulator is going to see more use. Only a small
    code clean up, can properly modularize things down the road if and when
    this code is needed again.

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,6 +1,133 @@
 use std::fs;
 
-fn parse_opcodes(filename: &str) -> Vec<i64> {
+#[derive(Debug)]
+enum OpCode {
+    ADD,
+    MUL,
+    IN,
+    OUT,
+    JNZ,
+    JZ,
+    LT,
+    EQ,
+    STOP
+}
+
+const OPCODE_LENGTH: usize = 1;
+
+#[derive(Debug)]
+enum OpParamMode {
+    POINTER,
+    VALUE
+}
+
+#[derive(Debug)]
+struct OpParam {
+    mode: OpParamMode,
+    offset: usize
+}
+
+impl OpParam {
+    fn new(mode: OpParamMode, offset: usize) -> OpParam {
+        OpParam{ mode, offset }
+    }
+
+    fn read(&self, memory: &Vec<i64>, address: usize) -> i64 {
+        match &self.mode {
+            OpParamMode::POINTER => memory[memory[address + self.offset + OPCODE_LENGTH] as usize],
+            OpParamMode::VALUE => memory[address + self.offset + OPCODE_LENGTH],
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Op {
+    code: OpCode,
+    params: Vec<OpParam>,
+}
+
+impl Op {
+    fn new(memory: &Vec<i64>, address: usize) -> Op {
+        let code = memory[address] % 100; // Last 2 digits
+        let parameter_modes = (memory[address] / 100)
+            .to_string()
+            .chars()
+            .map(|n| n.to_digit(10).unwrap() as usize)
+            .rev()
+            .collect::<Vec<_>>(); // Preceeding digits
+        let parse_parameter = |modes: &Vec<usize>, index: usize| -> OpParam {
+            let mode = match modes.get(index).unwrap_or(&0) {
+                0 => OpParamMode::POINTER,
+                1 => OpParamMode::VALUE,
+                _ => panic!("Unknown parameter mode"),
+            };
+            OpParam::new(mode, index)
+        };
+        let (code, params) = match code {
+            1 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+                let cx = OpParam::new(OpParamMode::VALUE, 2);
+
+                (OpCode::ADD, vec![ax, bx, cx])
+            }
+            2 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+                let cx = OpParam::new(OpParamMode::VALUE, 2);
+
+                (OpCode::MUL, vec![ax, bx, cx])
+            }
+            3 => {
+                let ax = OpParam::new(OpParamMode::VALUE, 0);
+
+                (OpCode::IN, vec![ax])
+            }
+            4 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+
+                (OpCode::OUT, vec![ax])
+            }
+            5 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+
+                (OpCode::JNZ, vec![ax, bx])
+            }
+            6 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+
+                (OpCode::JZ, vec![ax, bx])
+            }
+            7 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+                let cx = OpParam::new(OpParamMode::VALUE, 2);
+
+                (OpCode::LT, vec![ax, bx, cx])
+            }
+            8 => {
+                let ax = parse_parameter(&parameter_modes, 0);
+                let bx = parse_parameter(&parameter_modes, 1);
+                let cx = OpParam::new(OpParamMode::VALUE, 2);
+
+                (OpCode::EQ, vec![ax, bx, cx])
+            }
+            99 => (OpCode::STOP, vec![]),
+            _ => {
+                panic!("Unknown op: {}", code);
+            }
+        };
+        Op{ code, params }
+    }
+
+    fn len(&self) -> usize {
+        OPCODE_LENGTH + self.params.len()
+    }
+}
+
+fn parse_rom(filename: &str) -> Vec<i64> {
     let input = fs::read_to_string(filename).unwrap();
     input
         .split(",")
@@ -8,117 +135,107 @@ fn parse_opcodes(filename: &str) -> Vec<i64> {
         .collect()
 }
 
-fn get_parameter(
-    modes: &Vec<usize>,
-    index: usize,
-    stack_pointer: usize,
-    opcodes: &Vec<i64>,
-) -> i64 {
-    let mode = modes.get(index).unwrap_or(&0);
-    match mode {
-        0 => opcodes[opcodes[stack_pointer + index] as usize],
-        1 => opcodes[stack_pointer + index],
-        _ => panic!("Unknown parameter mode"),
-    }
+#[derive(Default)]
+struct Cpu {
+    memory: Vec<i64>,
+    ax: i64,
+    bx: i64,
+    cx: i64,
+    sp: usize,
 }
 
-fn run(opcodes: &Vec<i64>, input: i64) -> Vec<i64> {
-    let mut opcodes = opcodes.to_vec();
-    let mut stack_pointer: usize = 0;
-    let mut outputs: Vec<i64> = vec![];
-    loop {
-        let op = opcodes[stack_pointer] % 100; // Last 2 digits
-        let parameter_modes = (opcodes[stack_pointer] / 100)
-            .to_string()
-            .chars()
-            .map(|n| n.to_digit(10).unwrap() as usize)
-            .rev()
-            .collect::<Vec<_>>(); // Preceeding digits
-        stack_pointer += 1;
-        match op {
-            1 => {
-                // add
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
-                let cx = opcodes[stack_pointer + 2];
+impl Cpu {
+    fn new(memory: Vec<i64>) -> Cpu {
+        Cpu { memory, ..Default::default() }
+    }
 
-                stack_pointer += 3;
-                opcodes[cx as usize] = ax + bx;
-            }
-            2 => {
-                // mul
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
-                let cx = opcodes[stack_pointer + 2];
+    fn clear_registers(&mut self) {
+        self.ax = 0;
+        self.bx = 0;
+        self.cx = 0;
+        self.sp = 0;
+    }
 
-                stack_pointer += 3;
-                opcodes[cx as usize] = ax * bx;
-            }
-            3 => {
-                // in
-                let ax = opcodes[stack_pointer];
+    fn run(&mut self, input: i64) -> Vec<i64> {
+        self.clear_registers();
+        let mut outputs: Vec<i64> = vec![];
 
-                stack_pointer += 1;
-                opcodes[ax as usize] = input;
-            }
-            4 => {
-                // out
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
+        loop {
+            let op = Op::new(&self.memory, self.sp);
+            match op.code {
+                OpCode::ADD => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
+                    self.cx = op.params[2].read(&self.memory, self.sp);
 
-                stack_pointer += 1;
-                outputs.push(ax);
-            }
-            5 => {
-                // jnz
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
+                    self.memory[self.cx as usize] = self.ax + self.bx;
+                    self.sp += op.len();
+                }
+                OpCode::MUL => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
+                    self.cx = op.params[2].read(&self.memory, self.sp);
 
-                stack_pointer = match ax {
-                    0 => stack_pointer + 2,
-                    _ => bx as usize,
-                };
-            }
-            6 => {
-                // jz
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
+                    self.memory[self.cx as usize] = self.ax * self.bx;
+                    self.sp += op.len();
+                }
+                OpCode::IN => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
 
-                stack_pointer = match ax {
-                    0 => bx as usize,
-                    _ => stack_pointer + 2,
-                };
-            }
-            7 => {
-                // lt
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
-                let cx = opcodes[stack_pointer + 2];
+                    self.memory[self.ax as usize] = input;
+                    self.sp += op.len();
+                }
+                OpCode::OUT => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
 
-                stack_pointer += 3;
-                opcodes[cx as usize] = if ax < bx { 1 } else { 0 };
-            }
-            8 => {
-                // eq
-                let ax = get_parameter(&parameter_modes, 0, stack_pointer, &opcodes);
-                let bx = get_parameter(&parameter_modes, 1, stack_pointer, &opcodes);
-                let cx = opcodes[stack_pointer + 2];
+                    outputs.push(self.ax);
+                    self.sp += op.len();
+                }
+                OpCode::JNZ => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
 
-                stack_pointer += 3;
-                opcodes[cx as usize] = if ax == bx { 1 } else { 0 };
-            }
-            99 => break,
-            _ => {
-                println!("Unknown op: {}", op);
+                    self.sp = match self.ax {
+                        0 => self.sp + op.len(),
+                        _ => self.bx as usize,
+                    };
+                }
+                OpCode::JZ => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
+
+                    self.sp = match self.ax {
+                        0 => self.bx as usize,
+                        _ => self.sp + op.len(),
+                    };
+                }
+                OpCode::LT => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
+                    self.cx = op.params[2].read(&self.memory, self.sp);
+
+                    self.memory[self.cx as usize] = if self.ax < self.bx { 1 } else { 0 };
+                    self.sp += op.len();
+                }
+                OpCode::EQ => {
+                    self.ax = op.params[0].read(&self.memory, self.sp);
+                    self.bx = op.params[1].read(&self.memory, self.sp);
+                    self.cx = op.params[2].read(&self.memory, self.sp);
+
+                    self.memory[self.cx as usize] = if self.ax == self.bx { 1 } else { 0 };
+                    self.sp += op.len();
+                }
+                OpCode::STOP => break
             }
         }
+        outputs
     }
-    outputs
 }
 
 fn main() {
-    let opcodes = parse_opcodes("input");
+    let rom = parse_rom("input");
     println!("5-1:");
-    println!("{:?}", run(&opcodes, 1));
+    println!("{:?}", Cpu::new(rom.to_vec()).run(1));
     println!("5-2:");
-    println!("{:?}", run(&opcodes, 5));
+    println!("{:?}", Cpu::new(rom.to_vec()).run(5));
 }
